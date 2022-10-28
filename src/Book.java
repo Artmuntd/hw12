3bda50fe062d249dacf12aa214be3e22e726a37b
artem <artemkbnv@yandex.ru>
2022-10-28 12:18:14 +0300

    замечание исправлено

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,15 @@
 public class Book {
     private Author author;
+
+    private  String name;
     private int year;
-    public Book(  Author author, int year){
+
+
+    public Book(  Author author, int year, String name){
 
         this.author = author;
         this.year=year;
+        this.name = name;
 
     }
     public  Author getAuthor(){
@@ -12,6 +17,10 @@ public class Book {
     }
     public int getYear(){
         return this.year;
+    }
+
+    public String getName(){
+        return this.name;
     }
     public  void setYear( int year){
         this.year = year;
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Author firstAuthor = new Author("Лев", "Толстой");
         Author secondAuthor = new Author("Александр", "Пушкин");
-        Book firstBook = new Book(firstAuthor,1865);
-        Book secondBook = new Book(secondAuthor, 1836);
+        Book firstBook = new Book(firstAuthor,1865,"Война и мир");
+        Book secondBook = new Book(secondAuthor, 1836,"Евгений Онегин");
         firstBook.setYear(1877);
-        System.out.println("firstAuthor = " + firstAuthor.getName() + " " + firstAuthor.getSurname() + "  firstBook = "  +firstBook.getYear());
-        System.out.println("secondAuthor = " + secondAuthor.getName() + " " + secondAuthor.getSurname() + " secondBook = " + secondBook.getYear());
+        System.out.println("firstAuthor = " + firstAuthor.getName() + " " + firstAuthor.getSurname() + "  firstBook = "  +firstBook.getYear() + " " + firstBook.getName());
+        System.out.println("secondAuthor = " + secondAuthor.getName() + " " + secondAuthor.getSurname() + " secondBook = " + secondBook.getYear() +  " " + secondBook.getName());
     }
 }
